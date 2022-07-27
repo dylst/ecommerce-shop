7cd3df67d7a6c0e74ce32b4b377a2c743d2bc331
dylst <cdylan127@gmail.com>
2022-07-26 20:15:50 -0700

    Implement dynamic ProductDetails page
    Implemented a dynamic product details page that is displayed based on the product that the user clicked.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Head>
-        <title>E-Commerce Store</title>
+        <title>DYLST Headphones</title>
       </Head>
       <header>
         <Navbar />
