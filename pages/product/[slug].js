9ad5985136b7fe0cdae16724c8f32f614cf574ff
dylst <cdylan127@gmail.com>
2022-07-28 21:11:59 -0700

    Add buy now functionality and styling fixes
    Implemented the buy now button functionality in product details, as well as touched up some styling to improve responsiveness.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -13,7 +13,14 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decreaseQty, increaseQty, qty, onAdd } = useStateContext();
+  const { decreaseQty, increaseQty, qty, onAdd, setShowCart } =
+    useStateContext();
+
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  };
 
   return (
     <div>
@@ -74,7 +81,7 @@ const ProductDetails = ({ product, products }) => {
             >
               Add to Cart
             </button>
-            <button type='button' className='buy-now' onClick={() => {}}>
+            <button type='button' className='buy-now' onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
